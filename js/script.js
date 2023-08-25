0acef3da0c9520456970084002d495193aa746f1
Etzelaire <108952133+Etzelaire@users.noreply.github.com>
2023-08-25 21:12:17 +0300

    dfghjkhgf

--- js/script.js
+++ js/script.js
@@ -183,26 +183,8 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   }
 });
-///////////////////////////////////////
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions/////
-// function checkFlexGap() {
-//   var flex = document.createElement("div");
-//   flex.style.display = "flex";
-//   flex.style.flexDirection = "column";
-//   flex.style.rowGap = "1px";
 
-//   flex.appendChild(document.createElement("div"));
-//   flex.appendChild(document.createElement("div"));
 
-//   document.body.appendChild(flex);
-//   var isSupported = flex.scrollHeight === 1;
-//   flex.parentNode.removeChild(flex);
-//   console.log(isSupported);
-
-//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
-// }
-// checkFlexGap();
 document.addEventListener('DOMContentLoaded', (event) => {
   // Get the hamburger button, close button and the nav menu
   // var hamburger = document.querySelector(".icon-mobile-nav[name='menu-outline']");
@@ -251,6 +233,10 @@ document.addEventListener("DOMContentLoaded", function() {
       mainNav.classList.remove('active');
   });
 });
+
+
+/********************************************************/
+
 
  
 
